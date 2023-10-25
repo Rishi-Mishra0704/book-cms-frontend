ab0ea93c82e9afe5bbc552bed22ac08f6ef97286
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-26 01:13:08 +0530

    update sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
-import React, { useState, createContext } from "react";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 const inter = Inter({ subsets: ["latin"] });
-
-// Create an initial value for the context
-const initialAuthState = {
-  isLoggedIn: false,
-  setIsLoggedIn: () => {}, // You can provide a dummy function initially
-};
-
-// Create the AuthContext with an initial value
-export const AuthContext = createContext(initialAuthState);
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,11 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <AuthContext.Provider value={initialAuthState}>
         <body className={inter.className}>
           <Sidebar>{children}</Sidebar>
         </body>
-      </AuthContext.Provider>
     </html>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,20 +5,9 @@ import { BiSolidBook } from "react-icons/bi";
 
 interface SidebarProps {
     children: React.ReactNode;
-}
-
-// Create an authentication context
-const AuthContext = React.createContext<{ isLoggedIn: boolean } | undefined>(undefined);
+    }
 
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
-  // Use the context to get the user's authentication status
-  const authContext = useContext(AuthContext);
-  if (!authContext) {
-    // Handle the case where the context is not provided properly
-    return null;
-  }
-
-  const { isLoggedIn } = authContext;
 
   return (
     <div className="flex">
@@ -31,7 +20,7 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
           </Link>
           <span className="border-b-[1px] border-gray-300 w-full p-2"></span>
 
-          {isLoggedIn ? ( // Conditionally render based on authentication status
+         
             <>
               <Link href="/books">
                 <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
@@ -49,20 +38,7 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
                 </div>
               </Link>
             </>
-          ) : (
-            <>
-              <Link href="/signup">
-                <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-                  Sign Up
-                </div>
-              </Link>
-              <Link href="/login">
-                <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-                  Login
-                </div>
-              </Link>
-            </>
-          )}
+          
         </div>
       </div>
       <main className="ml-20 w-full">{children}</main>
