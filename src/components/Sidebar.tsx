e8fd9f6634c58fd2566bcefa74b766c2ed8c44bf
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-26 00:39:33 +0530

    initial setup

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+import { RxSketchLogo, RxDashboard, RxExit } from "react-icons/rx";
+import { BiSolidBook } from "react-icons/bi";
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <div className="bg-purple-800 text-white p-3 rounded-2xl inline-block">
+              <RxSketchLogo size={20} />
+            </div>
+          </Link>
+          <span className="border-b-[1px] border-gray-300 w-full p-2"></span>
+          <Link href="/">
+            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
+              <BiSolidBook size={20} />
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
+              <RxDashboard size={20} />
+            </div>
+          </Link>
+        </div>
+        <div>
+          <Link href="http://127.0.0.1:8000/logout">
+            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
+              <RxExit size={20} />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <main className="ml-20 w-full">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
